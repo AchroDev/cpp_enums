b06c0966d65fbfd4c364f876c9078858f80d0e6c
AchroDev <achrodev@proton.me>
2024-09-22 01:04:31 -0500

    Fixed naming collision error between the enum values and classes

--- log.cpp
+++ log.cpp
@@ -8,14 +8,14 @@ public:
     enum Level
     {
         // The '=0' is not necessary, helps with readability
-        Error = 0,
-        Warning,
-        Info
+        LevelError = 0,
+        LevelWarning,
+        LevelInfo
     };
 
     // private member variable to maintain the log level only accessible to the class
 private:
-    Level m_LogLevel = Info; // Now as the type 'Level' the value is require to be one of the enum values
+    Level m_LogLevel = LevelInfo; // Now as the type 'Level' the value is require to be one of the enum values
 
     // notice that public is written here again, separating the public methods from the public variables
 public:
@@ -26,19 +26,19 @@ public:
 
     void Warn(const char *message)
     {
-        if (m_LogLevel >= Warning)
+        if (m_LogLevel >= LevelWarning)
             std::cout << "[WARNING]: " << message << std::endl; // defining how the warning message should be displayed
     }
 
     void Error(const char *message)
     {
-        if (m_LogLevel >= Error)
+        if (m_LogLevel >= LevelError)
             std::cout << "[ERROR]: " << message << std::endl; // defining how the error message should be displayed
     }
 
     void Info(const char *message)
     {
-        if (m_LogLevel >= Info)
+        if (m_LogLevel >= LevelInfo)
             std::cout << "[INFO]: " << message << std::endl; // defining how the information message should be displayed
     }
 };
@@ -46,7 +46,7 @@ public:
 int main()
 {
     Log log;                             // Initiating the log
-    log.SetLevel(Log::Error);            // Setting a warning level for the log messages
+    log.SetLevel(Log::LevelError);       // Setting a warning level for the log messages
     log.Warn("A Warning!!");             // Example/Test warning message
     log.Error("An Error!!!");            // Example error message
     log.Info("Just some information.."); // Example info message
