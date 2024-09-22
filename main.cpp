9fae380cef9bafad965c6703434129ab1998e571
AchroDev <achrodev@proton.me>
2024-09-21 23:25:32 -0500

    Added comment on Enums and examples

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
-int main(){
+// Enum/Enumerations is a set of values, i.e. making a group of ints (a,b,c) instead of individually making the integers.
+
+// group of integers example
+// int A = 0;
+// int B = 1;
+// int C = 2;
+
+enum Example
+{
+    A,
+    B,
+    C
+};
+
+int main()
+{
     LOG("Hello World!");
     std::cin.get();
 }
