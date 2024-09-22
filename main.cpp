23d51ef802c9ce5db8e39b681e9c76b82a738ddc
AchroDev <achrodev@proton.me>
2024-09-22 00:28:14 -0500

    Fixed code and uncommented needed integers for example

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 // Enum/Enumerations is a set of values, i.e. making a group of ints (a,b,c) instead of individually making the integers.
 
 // group of integers example
-// int A = 0;
-// int B = 1;
-// int C = 2;
+int a = 0;
+int b = 1;
+int c = 2;
 
 enum Example
 {
-    a,
-    b,
-    c
+    A,
+    B,
+    C
 };
 
 int main()
